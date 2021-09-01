3d53fdf5b6dd35d7cc4ebf87cb5cb524fdb238f6
hifinerd <julianspqr@gmail.com>
2021-09-01 12:30:57 -0400

    add files

---
+++ spectagram/screens/CreatePost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class CreatePost extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>CreatePost</Text>
+            </View>
+        )
+    }
+}
